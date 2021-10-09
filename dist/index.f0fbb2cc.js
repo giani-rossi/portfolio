3503132a08cf34efdbc17a2fe510677dc123c0ea
Gianina Sol Rossi <gianinarossi@Gianinas-MacBook-Pro.local>
2021-10-09 18:40:44 -0300

    projects added with modal

--- dist/index.f0fbb2cc.js
+++ dist/index.f0fbb2cc.js
@@ -457,60 +457,87 @@ function hmrAcceptRun(bundle, id) {
 },{}]},["eCLlZ"], null, "parcelRequire32ad")
 //jQuery requirement
 $(document).ready(function() {
+    class Project {
+        constructor(title, description, link){
+            this.title = title;
+            this.description = description;
+            this.link = link;
+        }
+        callProject1() {
+            document.querySelector("#title1").innerText = project1.title;
+            document.querySelector("#description1").innerText = project1.description;
+        }
+        callProject2() {
+            console.log(project2.title);
+            document.querySelector("#title2").innerHTML = project2.title;
+            document.querySelector("#description2").innerText = project2.description;
+        }
+        callProject3() {
+            document.querySelector("#title3").innerText = project3.title;
+            document.querySelector("#description3").innerText = project3.description;
+        }
+    }
+    const project1 = new Project("AMADOR WEBSITE", "Lorem ipsum dolor sit amet consectetur adipisicing elit Sed saepe cumque similique\
+debitis assumenda aspernatur consequatur asperiores eum dignissimos officiis", "www.amador.com");
+    const project2 = new Project("ALQUILERES TEMPORALES WEBSITE", "Lorem ipsum dolor sit amet consectetur adipisicing elit Sed saepe cumque similique\
+debitis assumenda aspernatur consequatur asperiores eum dignissimos officiis", "www.alquileres-temporales.com");
+    const project3 = new Project("MAKEUP ARTIST WEBSITE", "Lorem ipsum dolor sit amet consectetur adipisicing elit Sed saepe cumque similique\
+debitis assumenda aspernatur consequatur asperiores eum dignissimos officiis", "www.makeup-artist.com");
     $("#open1").on("click", function() {
-        $(".cardOpen").addClass("active");
-        $('.active').css('position', 'fixed');
-        $('.active').css('display', 'block');
-        $('.active').css('z-index', '5');
+        $(".infoCard1").addClass("active");
+        $(".active").css("position", "fixed");
+        $(".active").css("display", "block");
+        $(".active").css("z-index", "5");
         //$('.active').css('width', '$(window).width()')
-        $('.active').css('top', '0');
-        $('.active').css('bottom', '0');
-        $('.active').css('left', '0');
+        $(".active").css("top", "0");
+        $(".active").css("bottom", "0");
+        $(".active").css("left", "0");
+        project1.callProject1();
         return false;
     });
     $("#close1").on("click", function() {
-        $(".cardOpen").removeClass("active");
-        $('.infoCard1').css('display', 'none');
-        $('.infoCard2').css('display', 'none');
-        $('.infoCard3').css('display', 'none');
+        $(".infoCard1").removeClass("active");
+        $(".infoCard1").css("display", "none");
+        $(".infoCard2").css("display", "none");
+        $(".infoCard3").css("display", "none");
         return false;
     });
     $("#open2").on("click", function() {
-        $(".cardOpen").addClass("active");
-        $(".cardOpen").addClass("active");
-        $('.active').css('position', 'fixed');
-        $('.active').css('display', 'block');
-        $('.active').css('z-index', '3');
+        $(".infoCard2").addClass("active");
+        $(".active").css("position", "fixed");
+        $(".active").css("display", "block");
+        $(".active").css("z-index", "5");
         //$('.active').css('width', '$(window).width()')
-        $('.active').css('top', '0');
-        $('.active').css('bottom', '0');
-        $('.active').css('left', '0');
+        $(".active").css("top", "0");
+        $(".active").css("bottom", "0");
+        $(".active").css("left", "0");
+        project2.callProject2();
         return false;
     });
     $("#close2").on("click", function() {
-        $(".cardOpen").removeClass("active");
-        $('.infoCard1').css('display', 'none');
-        $('.infoCard2').css('display', 'none');
-        $('.infoCard3').css('display', 'none');
+        $(".infoCard2").removeClass("active");
+        $(".infoCard1").css("display", "none");
+        $(".infoCard2").css("display", "none");
+        $(".infoCard3").css("display", "none");
         return false;
     });
     $("#open3").on("click", function() {
-        $(".cardOpen").addClass("active");
-        $(".cardOpen").addClass("active");
-        $('.active').css('position', 'fixed');
-        $('.active').css('display', 'block');
-        $('.active').css('z-index', '4');
+        $(".infoCard3").addClass("active");
+        $(".active").css("position", "fixed");
+        $(".active").css("display", "block");
+        $(".active").css("z-index", "5");
         //$('.active').css('width', '$(window).width()')
-        $('.active').css('top', '0');
-        $('.active').css('bottom', '0');
-        $('.active').css('left', '0');
+        $(".active").css("top", "0");
+        $(".active").css("bottom", "0");
+        $(".active").css("left", "0");
+        project3.callProject3();
         return false;
     });
     $("#close3").on("click", function() {
-        $(".cardOpen").removeClass("active");
-        $('.infoCard1').css('display', 'none');
-        $('.infoCard2').css('display', 'none');
-        $('.infoCard3').css('display', 'none');
+        $(".infoCard3").removeClass("active");
+        $(".infoCard1").css("display", "none");
+        $(".infoCard2").css("display", "none");
+        $(".infoCard3").css("display", "none");
         return false;
     });
 });
